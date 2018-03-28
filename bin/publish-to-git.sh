2e4b0924fa4248604692cede80f57d7ea47b1d7a
Aaron D Borden <adborden@a14n.net>
2018-03-28 10:10:08 -0700

    Add cron job to sync to git backend

---
+++ bin/publish-to-git.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# publish-to-git.sh
+#
+# Pull changes from git and sync them to the database.
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+bin/configure-ssh.sh
+
+# TODO iterate over all projects with pootle list_projects
+project=usa-gov-example
+pootle fs fetch "$project"
+pootle fs add "$project"
+pootle fs resolve --overwrite --pootle-wins "$project"
+pootle fs sync "$project"
